c12b2f348119ef0d4bd2ad565efd30a72823f1d1
davidHooper-WCCI <79227061+davidHooper-WCCI@users.noreply.github.com>
2021-03-11 18:43:45 -0700

    Update VirtualPet.spec.js
    removed extra undertest

--- src/js/VirtualPet.spec.js
+++ src/js/VirtualPet.spec.js
@@ -18,7 +18,6 @@ beforeEach(()=>{
     describe("Virtual Pets have a name", ()=>{
         describe("Constructing virtual pet name is successful",()=>{
             it("Virtual Pet is named Fishy", ()=>{
-                underTest.name = "Fishy";
                 expect(underTest.name).toBe("Fishy");
             });
         });
